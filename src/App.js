72e3c7d729e96b27f3fedfc119f84cb667a45857
varunmind-art <varun.mind@gmail.com>
2025-10-08 20:23:43 +0530

    Add main App.js for React dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
