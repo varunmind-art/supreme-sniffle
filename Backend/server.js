96e8b404073c9f71f4bc97726ecad18a5a492181
varunmind-art <varun.mind@gmail.com>
2025-10-09 22:16:36 +0530

    Change server port from 5000 to 5001

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use("/api", apiRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/delta-bot";
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
